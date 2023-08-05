0351e35bc0c05d6bb0a4cead31fe034721fad417
Rahul Ramesh <60769128+rahulramesh96@users.noreply.github.com>
2023-08-05 10:44:33 +0100

    Update localfi.py

--- localfi.py
+++ localfi.py
@@ -33,9 +33,9 @@ def main():
 
     for word in custom_dictionary:
         for payload in payloads:
-            fuzzed_url = args.url.replace("FUZZ", f"{word}={payload}")
+            fuzzed_url = args.url.replace("FUZZ", word).replace("PAYLOAD", payload)
             if test_vulnerability(fuzzed_url):
-                print(f"Vulnerable to LFI: Word: {word.split('=')[1]}, Payload: {payload}, Fuzzed URL: {fuzzed_url}")
+                print(f"Vulnerable to LFI: Word: {word}, Payload: {payload}, Fuzzed URL: {fuzzed_url}")
 
 if __name__ == "__main__":
     main()
