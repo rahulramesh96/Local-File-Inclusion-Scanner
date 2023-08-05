5e1930c341fcaa4d7cb5748f108fda0a869d5e2f
Rahul Ramesh <60769128+rahulramesh96@users.noreply.github.com>
2023-08-05 10:30:57 +0100

    Update localfi.py

--- localfi.py
+++ localfi.py
@@ -35,7 +35,7 @@ def main():
         for payload in payloads:
             full_url = f"{args.url}{word}={payload}"
             if test_vulnerability(full_url):
-                print(f"Vulnerable to LFI: Word: {word}, Payload: {payload}, URL: {full_url}")
+                print(f"Vulnerable to LFI: Word: {word.split('=')[1]}, Payload: {payload}, URL: {full_url}")
 
 if __name__ == "__main__":
     main()
